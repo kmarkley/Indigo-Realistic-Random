2ff2ec4a57e3b81d7d401c967a20fd60417312fe
kmarkley <kirk@kirkmarkleydesign.com>
2017-07-04 09:50:28 -0500

    GitHubPluginUpdater

--- Realistic Random.indigoPlugin/Contents/Server Plugin/plugin.py
+++ Realistic Random.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -7,6 +7,7 @@ import indigo
 import time
 import datetime
 import random
+from ghpu import GitHubPluginUpdater
 
 
 # Note the "indigo" module is automatically imported and made available inside
@@ -23,13 +24,16 @@ lightDictKeys = (
     'maxDuration',
     )
 
+k_updateCheckHours = 24
+
 ################################################################################
 class Plugin(indigo.PluginBase):
-    
+
     #-------------------------------------------------------------------------------
     def __init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs):
         indigo.PluginBase.__init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs)
-    
+        self.updater = GitHubPluginUpdater(self)
+
     def __del__(self):
         indigo.PluginBase.__del__(self)
 
@@ -37,6 +41,7 @@ class Plugin(indigo.PluginBase):
     # Start, Stop and Config changes
     #-------------------------------------------------------------------------------
     def startup(self):
+        self.nextCheck = self.pluginPrefs.get('nextUpdateCheck',0)
         self.debug = self.pluginPrefs.get("showDebugInfo",False)
         self.logger.debug("startup")
         if self.debug:
@@ -46,6 +51,7 @@ class Plugin(indigo.PluginBase):
     #-------------------------------------------------------------------------------
     def shutdown(self):
         self.logger.debug("shutdown")
+        self.pluginPrefs['nextUpdateCheck'] = self.nextCheck
         self.pluginPrefs["showDebugInfo"] = self.debug
 
     #-------------------------------------------------------------------------------
@@ -63,10 +69,12 @@ class Plugin(indigo.PluginBase):
                 loopTime = time.time()
                 for devId, dev in self.deviceDict.items():
                     dev.update()
+                if loopTime > self.nextCheck:
+                    self.checkForUpdates()
                 self.sleep(loopTime+5-time.time())
         except self.StopThread:
             pass
-    
+
     #-------------------------------------------------------------------------------
     # Device Methods
     #-------------------------------------------------------------------------------
@@ -76,19 +84,19 @@ class Plugin(indigo.PluginBase):
             self.updateDeviceVersion(dev)
         if dev.configured:
             self.deviceDict[dev.id] = self.Randomizer(dev, self)
-    
+
     #-------------------------------------------------------------------------------
     def deviceStopComm(self, dev):
         self.logger.debug("deviceStopComm: "+dev.name)
         if dev.id in self.deviceDict:
             #self.deviceDict[dev.id].cancel()
             del self.deviceDict[dev.id]
-    
+
     #-------------------------------------------------------------------------------
     def validateDeviceConfigUi(self, valuesDict, typeId, devId, runtime=False):
         self.logger.debug("validateDeviceConfigUi: " + typeId)
         errorsDict = indigo.Dict()
-        
+
         lightsList = []
         for idx in ("{:0>2d}".format(i) for i in range(1,11)):
             lightId = valuesDict.get('devId'+idx,'')
@@ -104,12 +112,12 @@ class Plugin(indigo.PluginBase):
                             errorsDict[key+idx] = "Must be a positive integer"
                         elif not ( 0 < int(valuesDict.get(key+idx)) < 481):
                             errorsDict[key+idx] = "Must be between 1 and 480"
-        
+
         if len(errorsDict) > 0:
             self.logger.debug('validate device config error: \n%s' % str(errorsDict))
             return (False, valuesDict, errorsDict)
         return (True, valuesDict)
-    
+
     #-------------------------------------------------------------------------------
     def updateDeviceVersion(self, dev):
         theProps = dev.pluginProps
@@ -124,7 +132,7 @@ class Plugin(indigo.PluginBase):
         # push to server
         theProps["version"] = self.pluginVersion
         dev.replacePluginPropsOnServer(theProps)
-    
+
     #-------------------------------------------------------------------------------
     # Action Methods
     #-------------------------------------------------------------------------------
@@ -147,23 +155,36 @@ class Plugin(indigo.PluginBase):
         # UNKNOWN
         else:
             self.logger.debug('"{}" {} request ignored'.format(dev.name, unicode(action.deviceAction)))
-    
+
     #-------------------------------------------------------------------------------
     def freezeRandomizerEffect(self, action):
         try:
             self.deviceDict[action.deviceId].cancel(False)
         except:
             self.logger.error('device "{}" not available'.format(action.deviceId))
-    
+
     #-------------------------------------------------------------------------------
     def forceRandomizerOff(self, action):
         try:
             self.deviceDict[action.deviceId].cancel(True)
         except:
             self.logger.error('device "{}" not available'.format(action.deviceId))
-    
+
     #-------------------------------------------------------------------------------
     # Menu Methods
+    #-------------------------------------------------------------------------------
+    def checkForUpdates(self):
+        self.updater.checkForUpdate()
+        self.nextCheck = time.time() + k_updateCheckHours*60*60
+
+    #-------------------------------------------------------------------------------
+    def updatePlugin(self):
+        self.updater.update()
+
+    #-------------------------------------------------------------------------------
+    def forceUpdate(self):
+        self.updater.update(currentVersion='0.0.0')
+
     #-------------------------------------------------------------------------------
     def toggleDebug(self):
         if self.debug:
@@ -172,7 +193,7 @@ class Plugin(indigo.PluginBase):
         else:
             self.debug = True
             self.logger.debug("Debug logging enabled")
-        
+
     #-------------------------------------------------------------------------------
     # Menu Callbacks
     #-------------------------------------------------------------------------------
@@ -183,13 +204,13 @@ class Plugin(indigo.PluginBase):
             if not dev.id in excludeList:
                 devList.append((dev.id, dev.name))
         devList.append((0,"- none -"))
-        return devList        
-        
+        return devList
+
     ###############################################################################
     # Classes
     ###############################################################################
     class Randomizer(object):
-        
+
         #-------------------------------------------------------------------------------
         def __init__(self, instance, plugin):
             self.dev        = instance
@@ -197,35 +218,35 @@ class Plugin(indigo.PluginBase):
             self.props      = self.dev.pluginProps
             self.states     = self.dev.states
             self.nextUpdate = 0
-            
+
             self.logger     = plugin.logger
-            
+
             self.lightsList = list()
             for index in range(1,11):
                 try:
                     self.lightsList.append(self.ControlledLight(self.props, index, self))
                 except:
                     pass
-    
+
         #-------------------------------------------------------------------------------
         def update(self):
             if self.onState:
                 for light in self.lightsList:
                     light.update()
                 self.nextUpdate = min(light.expire for light in self.lightsList)
-    
+
         #-------------------------------------------------------------------------------
         def cancel(self, turnOff=False):
             self.onState = False
             for light in self.lightsList:
                 light.cancel(turnOff)
-    
+
         #-------------------------------------------------------------------------------
         # Class Properties
         #-------------------------------------------------------------------------------
         def onStateGet(self):
             return self.states['onOffState']
-        
+
         def onStateSet(self,newState):
             if newState != self.onState:
                 self.logger.info('"{}" {}'.format(self.dev.name, ['off','on'][newState]))
@@ -233,12 +254,12 @@ class Plugin(indigo.PluginBase):
                 self.states = self.dev.states
                 if newState:
                     self.update()
-        
+
         onState = property(onStateGet, onStateSet)
-        
+
         ###############################################################################
         class ControlledLight(object):
-            
+
             #-------------------------------------------------------------------------------
             def __init__(self, props, index, parent):
                 indexString     = "{:0>2d}".format(index)
@@ -250,13 +271,13 @@ class Plugin(indigo.PluginBase):
                 self.maxDur     = int(props.get('maxDuration'+indexString,'60'))
                 self.expire     = 0
                 self.logger     = parent.logger
-        
+
             #-------------------------------------------------------------------------------
             def refresh(self):
                 self.dev        = indigo.devices[self.id]
                 self.name       = self.dev.name
                 self.onState    = self.dev.onState
-            
+
             #-------------------------------------------------------------------------------
             def update(self):
                 if self.expire < time.time():
@@ -271,7 +292,7 @@ class Plugin(indigo.PluginBase):
                     else:
                         indigo.device.turnOn(self.id, duration=randomDuration, delay=randomDelay)
                     self.expire = time.time() + randomDelay + randomDuration
-            
+
             #-------------------------------------------------------------------------------
             def cancel(self, turnOff=False):
                 self.refresh()
@@ -281,4 +302,3 @@ class Plugin(indigo.PluginBase):
                 if self.onState and turnOff:
                     self.logger.debug('turn off "{}"'.format(self.name))
                     indigo.device.turnOff(self.id)
-                
